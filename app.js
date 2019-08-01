a556dd4c7c249bd35c4764a37a0ca5c95e8c5d6f
Victor <vjmoyaparedes@gmail.com>
2019-07-31 22:18:55 -0300

    Terminada practica de conceptos basicos de vuejs

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const app = new Vue({
     el:'#app',
     data:{
-        titulo:'TAREAS QUE HACER'
+        titulo:'TAREAS QUE HACER',
+        tareas:[],
+        nuevaTarea:''
+
+    },
+    methods:{
+    agregarTarea: function(){
+         this.tareas.push({
+             nombre: this.nuevaTarea,
+             estado: false
+         });
+         this.nuevaTarea =' ';
+         localStorage.setItem('tareas-vue', JSON.stringify(this.tareas));
+        },
+    editarTarea:function(index){
+            this.tareas[index].estado=true;
+            localStorage.setItem('tareas-vue', JSON.stringify(this.tareas));
+        },
+    eliminarTarea:function(index){
+            this.tareas.splice(index ,1);
+            localStorage.setItem('tareas-vue', JSON.stringify(this.tareas));
     }
-})
+    },
+    created:function(){
+        let datosDB = JSON.parse(localStorage.getItem("tareas-vue"));
+        if(datosDB===null){
+            this.tareas = [];
+        }else{
+            this.tareas = datosDB;
+        }
+
+    }
+
+});
